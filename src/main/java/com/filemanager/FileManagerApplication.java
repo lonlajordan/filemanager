6988c174d554e1888c7b115e21bfecf02f367132
epsilon <gatienjordanlonlaep@gmail.com>
2022-09-13 17:00:28 +0100

    create admin user on app launch

--- src/main/java/com/filemanager/FileManagerApplication.java
+++ src/main/java/com/filemanager/FileManagerApplication.java
@@ -1,13 +1,26 @@
 package com.filemanager;
 
+import com.filemanager.enums.Role;
+import com.filemanager.models.User;
+import com.filemanager.repositories.SettingRepository;
+import com.filemanager.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
-public class FileManagerApplication extends SpringBootServletInitializer {
+public class FileManagerApplication extends SpringBootServletInitializer implements CommandLineRunner {
+    private final SettingRepository settingRepository;
+    private final UserRepository userRepository;
+
+    public FileManagerApplication(SettingRepository settingRepository, UserRepository userRepository) {
+        this.settingRepository = settingRepository;
+        this.userRepository = userRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FileManagerApplication.class, args);
@@ -16,5 +29,14 @@ public class FileManagerApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(FileManagerApplication.class);
+    }
+
+    @Override
+    public void run(String... args) {
+        User user = userRepository.findByUsername("admin");
+        if(user == null){
+            user = new User("admin", new BCryptPasswordEncoder().encode("admin@123"), Role.ROLE_ADMIN.name());
+            userRepository.save(user);
+        }
     }
 }
