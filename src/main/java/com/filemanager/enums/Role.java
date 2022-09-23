d14348fa73a2b3b71f528e9a1c64441dc87c1143
epsilon <gatienjordanlonlaep@gmail.com>
2022-09-23 16:20:11 +0100

    adding user crud

--- src/main/java/com/filemanager/enums/Role.java
+++ src/main/java/com/filemanager/enums/Role.java
@@ -1,10 +1,20 @@
 package com.filemanager.enums;
 
 public enum Role {
-    ROLE_ADMIN,
-    ROLE_GIE,
-    ROLE_CBC_MONET,
-    ROLE_CBC_INFO,
-    ROLE_CBT_MONET,
-    ROLE_CBT_INFO
+    ROLE_ADMIN("ADMINISTRATEUR"),
+    ROLE_GIE(""),
+    ROLE_CBC_MONET(""),
+    ROLE_CBC_INFO(""),
+    ROLE_CBT_MONET(""),
+    ROLE_CBT_INFO("");
+
+    private final String displayValue;
+
+    Role(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
