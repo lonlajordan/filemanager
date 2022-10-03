0f952467454a7e561757f31b15c7d232fe308edc
epsilon <gatienjordanlonlaep@gmail.com>
2022-10-03 15:19:41 +0100

    deleting logs issues

--- src/main/java/com/filemanager/repositories/LogRepository.java
+++ src/main/java/com/filemanager/repositories/LogRepository.java
@@ -5,10 +5,14 @@ import com.filemanager.models.Log;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface LogRepository  extends JpaRepository<Log, Long> , PagingAndSortingRepository<Log, Long> {
     Page<Log> findAllByOrderByDateDesc(Pageable pageable);
     int countAllByMessageContaining(String message);
-    int deleteAllByLevel(Level level);
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    void deleteAllByLevel(Level level);
 }
